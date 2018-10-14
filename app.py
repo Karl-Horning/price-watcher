b928a78f61617fb0548b91d175b75c61f38720b1
Karl Horning <k.horning@me.com>
2018-10-14 22:07:55 +0100

    Changed the main function so that it returns an object

--- app.py
+++ app.py
@@ -5,29 +5,34 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_price_from_amazon(url, best_price):
+    result = dict()
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     now = datetime.datetime.now()
-    current_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    # result['current_date_time'] = now.strftime("%Y-%m-%d %H:%M:%S")
+    result['current_date'] = now.strftime("%Y-%m-%d")
+    result['current_time'] = now.strftime("%H:%M:%S")
+    # current_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
     # Search the returned HTML for the title and the price
     title = soup.find(id='productTitle').string
+    # title = soup.find(id='productTitle').string
     price = soup.find(id='priceblock_ourprice').string
     # Remove the whitespace from the beginning and end of the title
-    title = title.strip()
+    result['title'] = title.strip()
     # Remove the pound sign and convert the price from a string to a decimal
-    price = float(
+    result['price'] = float(
         price.replace('£', '')
     )
 
     # Write the price to a text file
-    f = open("log.txt", "w")
-    f.write(f'{title}: £{price}')
+    # f = open("log.txt", "w")
+    # f.write(f'{title}: £{price}')
 
     # Append the price to a CSV file
-    with open("log.csv", "a") as price_file:
-        price_writer = csv.writer(price_file, delimiter=',')
-        price_writer.writerow([current_date_time, title, price])
+    # with open("log.csv", "a") as price_file:
+        # price_writer = csv.writer(price_file, delimiter=',')
+        # price_writer.writerow([current_date_time, title, price])
 
     # Decide whether or not to send the email
     # if price == best_price:
@@ -37,7 +42,8 @@ def get_price_from_amazon(url, best_price):
     # else:
     #     print('It\'s more expensive!')
 
-    return f'{title}: £{price}'
+    # return f'{title}: £{price}'
+    return result
 
 if __name__ == '__main__':
     print(get_price_from_amazon('https://www.amazon.co.uk/Super-Mario-Party-Nintendo-Switch/dp/B07DTNGK5V/ref=sr_1_3?s=videogames&ie=UTF8&qid=1539340023&sr=1-3', 42.99))
